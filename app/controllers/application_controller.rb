a2887c144dc10afbf3927d3e0868a4b131f571ed
ah100 <annieweihuang@gmail.com>
2016-10-12 03:06:05 +0000

    WeiH test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "hello, world! and Bill says... huzzah to 694...Andy was here! JK shuffles the deck. Mke D finishes it!"
+    render html: "hello, world! and Bill says... huzzah to 694...Andy was here! JK shuffles the deck. Mke D finishes it! Wei H"
   end
 end
