f36ccef17b8f809862d0ae528585881353e23e31
ah100 <annieweihuang@gmail.com>
2016-10-17 06:57:59 +0000

    modify activity screens 1-3

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,41 @@
 class ActivitiesController < ApplicationController
   def new
+    @activity = Activity.new
   end
 
   def edit
+    @activity = Activity.find(params[:id])
   end
+  
+  def create
+    @activity = Activity.new(activity_params)
+    if @activity.save
+      # flash[:success] = "Activty added successfully!"
+      redirect_to activities_url
+    else
+      render 'new'
+    end
+  end
+  
+  def update
+    @activity = Activity.find(params[:id])
+    if @activity.update(activity_params)
+       redirect_to activities_url
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+  
+  def index
+    @activities = Activity.all
+  end
+  
+  
+  private
+    def activity_params
+      params.required(:activity).permit(:name, :start_date, :end_date, :description,
+       :goal, :notes)
+    end
+  
 end
