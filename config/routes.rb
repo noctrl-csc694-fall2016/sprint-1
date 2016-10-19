bbb8a69af32326f6c10ec955a0ab915edf730bcd
Jason Kryst <jasonkryst@gmail.com>
2016-10-19 02:16:57 +0000

    Fixes routing bug

--- config/routes.rb
+++ config/routes.rb
@@ -21,8 +21,8 @@ Rails.application.routes.draw do
   get 'import-export/inkind'
   get 'import-export/export'
   
-  get    '/hyper-surf/donors', to: 'hyper_surf#donors'
-  get    '/hyper-surf/all', to: 'hyper_surf#all'
+  get    'hyper-surf/donors', to: 'hyper_surf#donors'
+  get    'hyper-surf/all', to: 'hyper_surf#all'
   
   root 'static_pages#home'
 end
