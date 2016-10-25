3cef5ef9d49a540479bc8df99b2f5673253dfd7d
Pat Murphy <ptmurphy@noctrl.edu>
2016-10-25 20:59:28 +0000

    Model testing

--- test/models/donor_test.rb
+++ test/models/donor_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class DonorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @donor = Donor.new(id:999, first_name: "John", last_name: "Doe", 
+    address: "123 Fake St", address2: "", city: "Phonyville", state: "PA", 
+    zip: 43873, phone: "840-343-8778", email: "jd@yahoo.com", notes: "etc", 
+    created_at: "2016-10-23 17:30:16", updated_at: "2016-10-23 17:30:16")
+  end
+
+  
+  test "valid donor" do 
+    assert @donor.valid?
+  end
+
 end
